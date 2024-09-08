3b8de52224b4708aaff9f715f1080618391aa2c3
Guido Roncarolo <guido.roncarolo@gmail.com>
2024-09-08 17:16:17 +0200

    [#5] Add Deutch algoritm
    Signed-off-by: Guido Roncarolo <guido.roncarolo@gmail.com>

---
+++ oracles/test_deutch.py
@@ -0,0 +1,66 @@
+from deutch import deutch
+from sympy.physics.quantum.qubit import Qubit, matrix_to_qubit
+from sympy import sqrt
+from sympy.physics.quantum import TensorProduct
+from sympy.physics.quantum.represent import represent
+
+def test_const_0():
+    print()
+
+    def f(x):
+        return 0
+
+    r = deutch(f)
+    print('\nResult const zero function: {r}'.format(r=r))
+
+    truth = TensorProduct(Qubit(0), ((Qubit(0) - Qubit(1)) / sqrt(2)))
+    truth = matrix_to_qubit(represent(truth))
+    assert truth == r
+
+
+def test_const_1():
+    print()
+
+    def f(x):
+        return 1
+
+    r = deutch(f)
+    print('\nResult const one function: {r}'.format(r=r))
+
+    truth = TensorProduct(-Qubit(0), ((Qubit(0) - Qubit(1)) / sqrt(2)))
+    truth = matrix_to_qubit(represent(truth))
+    assert truth == r
+
+
+def test_f_balanced_0():
+    print()
+
+    def f(x):
+        if x[0] == 0:
+            return 1
+        else:
+            return 0
+
+    r = deutch(f)
+    print('\nResult balanced zero function: {r}'.format(r=r))
+
+    truth = TensorProduct(-Qubit(1), ((Qubit(0) - Qubit(1)) / sqrt(2)))
+    truth = matrix_to_qubit(represent(truth))
+    assert truth == r
+
+
+def test_f_balanced_1():
+    print()
+
+    def f(x):
+        if x[0] == 0:
+            return 0
+        else:
+            return 1
+
+    r = deutch(f)
+    print('\nResult balanced one function: {r}'.format(r=r))
+
+    truth = TensorProduct(Qubit(1), ((Qubit(0) - Qubit(1)) / sqrt(2)))
+    truth = matrix_to_qubit(represent(truth))
+    assert truth == r

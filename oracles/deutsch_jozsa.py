da35ad3dad7985dee73933037fe1a78df287ab01
Guido Roncarolo <guido.roncarolo@gmail.com>
2024-09-08 17:16:35 +0200

    [#5] Add Deutsch-Jozsa algoritm
    Signed-off-by: Guido Roncarolo <guido.roncarolo@gmail.com>

---
+++ oracles/deutsch_jozsa.py
@@ -0,0 +1,52 @@
+""" This module implements Deutch_Jozsa algorithm """
+from sympy.physics.quantum.qapply import qapply
+from sympy.physics.quantum.qubit import Qubit, matrix_to_qubit
+from sympy.physics.quantum import TensorProduct
+from sympy.physics.quantum.gate import HadamardGate
+from sympy.physics.quantum.represent import represent
+
+from oracle import oracle
+from util import hn
+
+
+def deutsch_jozsa(f, n):
+    """
+    Run  Deutsch Jozsa algorithm and return |x>*|y XOR f(x)>
+    :param func f: oracle function
+    :param int n: string length
+
+    The problem:
+    This is a generalization of Deutsch algorithm
+    consider f {0,1}^n  -> f {0, 1}^n that is not known
+    determine if f is balanced or constant
+    where f is
+    balanced if exactly half of the input go to zero ( and the other half to one)
+    constant if all input go to zero OR all got to one
+
+    The Solution:
+                         +-----------------+
+      |x>  |0>-/n--H*n---|                 |--/n--H*n---M |x>
+                         |       U_f       |
+      |y>  |1>---H-------|                 |------------  |y XOR f(x)>
+                         +-----------------+
+
+    Constant zero -> +|0*n>*|->
+    Constant  one -> -|0*n>*|->
+    Balanced 0->1 ->  NOT (+|0*n>*|->) and NOT (-|0*n>*|->)
+    """
+    x = qapply(hn(n) * Qubit('0' * n))
+    print(f"|x>: {x}")
+    y = qapply(HadamardGate(0) * Qubit(1))
+    print(f"|y>: {y}")
+
+    # calculate the Tensor Product of the inputs
+    xy = TensorProduct(x, y)
+    xy = matrix_to_qubit(represent(xy))
+    print(f"|xy>: {xy}")
+
+    # apply oracle
+    r = oracle(x, y, f)
+
+    # apply H*n on top Qubits
+    r = qapply(hn(n, start=1) * r)
+    return r
---
+++ oracles/test_deutsch_jozsa.py
@@ -0,0 +1,59 @@
+from sympy.physics.quantum.qapply import qapply
+from sympy.physics.quantum.qubit import Qubit, IntQubit, matrix_to_qubit, measure_partial
+from sympy import sqrt
+from sympy.physics.quantum import TensorProduct
+from sympy.physics.quantum.represent import represent
+
+from oracle import oracle
+from deutsch_jozsa import deutsch_jozsa
+
+
+def test_const_0():
+    print()
+
+    def f(x):
+        return 0
+
+    n = 3
+    r = deutsch_jozsa(f, n)
+    print(f'\nResult const zero function: {r}')
+
+    truth = TensorProduct(Qubit('0' * n), 1 / sqrt(2) * (Qubit(0) - Qubit(1)))
+    truth = matrix_to_qubit(represent((truth)))
+    assert truth == r
+
+
+def test_const_1():
+    print()
+
+    def f(x):
+        return 1
+
+    n = 3
+    r = deutsch_jozsa(f, n)
+    print(f'\nResult const one function: {r}')
+
+    truth = TensorProduct(-1 * Qubit('0' * n), 1 / sqrt(2) * (Qubit(0) - Qubit(1)))
+    truth = matrix_to_qubit(represent(truth))
+    assert truth == r
+
+
+def test_f_balanced():
+    print()
+
+    def f(x):
+        q = IntQubit(*x)
+        if q.as_int() % 2 == 0:
+            return 1
+        else:
+            return 0
+
+    n = 3
+    r = deutsch_jozsa(f, n)
+    print(f'\nResult balanced zero function: {r}')
+    truth_0 = TensorProduct(Qubit('0' * n), 1 / sqrt(2) * (Qubit(0) - Qubit(1)))
+    truth_0 = matrix_to_qubit(represent((truth_0)))
+    truth_1 = TensorProduct(-1 * Qubit('0' * n), 1 / sqrt(2) * (Qubit(0) - Qubit(1)))
+    truth_1 = matrix_to_qubit(represent(truth_1))
+
+    assert r != truth_0 and r != truth_1

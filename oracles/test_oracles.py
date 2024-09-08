50094fc5424a40317e84734453c7596149049e7d
Guido Roncarolo <guido.roncarolo@gmail.com>
2024-09-08 17:15:58 +0200

    [#5] Add basic Oracle code
    Signed-off-by: Guido Roncarolo <guido.roncarolo@gmail.com>

---
+++ oracles/test_oracles.py
@@ -0,0 +1,36 @@
+from sympy.physics.quantum.qapply import qapply
+from sympy.physics.quantum.qubit import Qubit, matrix_to_qubit
+from sympy import sqrt, simplify
+from sympy.physics.quantum import TensorProduct
+from sympy.physics.quantum.gate import HadamardGate
+from sympy.physics.quantum.represent import represent
+
+from oracle import oracle
+
+
+# Introduction to classical and quantum computing
+# Exercise 7.3
+
+def test_7_3():
+    # consider a superposed state
+    x = sqrt(3) / 2 * Qubit(0) + 1 / 2 * Qubit(1)
+    print("|x>={x}".format(x=x))
+
+    y = qapply(HadamardGate(0) * Qubit(1))
+    print("|y>={y}".format(y=y))
+
+    xy = TensorProduct(x, y)
+    xy = matrix_to_qubit(represent(xy))
+    print("|xy>={xy}".format(xy=xy))
+
+    r = simplify(oracle(x, y, lambda a: 0))
+    print('Result for constant zero function: r={r}'.format(r=r))
+    solution = simplify(
+        sqrt(3) / (2 * sqrt(2)) * (Qubit('00') - Qubit('01')) + 1 / (2 * sqrt(2)) * (Qubit('10') - Qubit('11')))
+    assert simplify(r - solution) == 0
+
+    r = oracle(x, y, lambda a: 1)
+    print('Result for constant one function: r={r}'.format(r=r))
+    solution = simplify(
+        sqrt(3) / (2 * sqrt(2)) * (Qubit('01') - Qubit('00')) + 1 / (2 * sqrt(2)) * (Qubit('11') - Qubit('10')))
+    assert simplify(r - solution) == 0

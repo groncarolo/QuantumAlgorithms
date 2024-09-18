8542dacfb6a0938510d52be53d5cf1c95301125b
Guido Roncarolo <guido.roncarolo@gmail.com>
2024-09-18 14:32:03 +0200

    Test Oracle: Add more basic tests
    Signed-off-by: Guido Roncarolo <guido.roncarolo@gmail.com>

--- oracles/test_oracles.py
+++ oracles/test_oracles.py
@@ -40,3 +40,155 @@ def test_7_3():
     solution = simplify(
         sqrt(3) / (2 * sqrt(2)) * (Qubit('01') - Qubit('00')) + 1 / (2 * sqrt(2)) * (Qubit('11') - Qubit('10')))
     assert simplify(r - solution) == 0
+
+
+def test_phase():
+    x = Qubit(0)
+    print(f"|x>={x}")
+
+    y = qapply(HadamardGate(0) * Qubit(1))
+    print(f"|y>={y}")
+
+    def f0(x, *args):
+        return 0
+
+    r = oracle(x, y, f0)
+    print(r)
+
+    truth = (-1) ** (f0(x)) * TensorProduct(x, y)
+    truth = matrix_to_qubit(represent(truth))
+    assert truth == r
+
+    def f1(x, *args):
+        return 1
+
+    r = oracle(x, y, f1)
+    print(r)
+
+    truth = (-1) ** (f1(x)) * TensorProduct(x, y)
+    truth = matrix_to_qubit(represent(truth))
+    assert truth == r
+
+
+def test_find_f0_f_const_0():
+    # consider a superposed state
+    x = Qubit(0)
+    print(f"|x>={x}")
+
+    y = Qubit(0)
+    print(f"|y>={y}")
+
+    def f(x, *args):
+        return 0
+
+    r = oracle(x, y, f)
+    print(r)
+
+    truth = TensorProduct(x, Qubit(f(0)))
+    truth = matrix_to_qubit(represent(truth))
+    assert truth == r
+
+def test_find_f1_f_const_0():
+    # consider a superposed state
+    x = Qubit(1)
+    print(f"|x>={x}")
+
+    y = Qubit(0)
+    print(f"|y>={y}")
+
+    def f(x, *args):
+        return 0
+
+    r = oracle(x, y, f)
+    print(r)
+
+    truth = TensorProduct(x, Qubit(f(1)))
+    truth = matrix_to_qubit(represent(truth))
+    assert truth == r
+
+
+
+def test_find_f0_f_const_1():
+    # consider a superposed state
+    x = Qubit(0)
+    print(f"|x>={x}")
+
+    y = Qubit(0)
+    print(f"|y>={y}")
+
+    def f(x, *args):
+        return 1
+
+    r = oracle(x, y, f)
+    print(r)
+
+    truth = TensorProduct(x, Qubit(f(0)))
+    truth = matrix_to_qubit(represent(truth))
+    assert truth == r
+
+def test_find_f1_f_const_1():
+    # consider a superposed state
+    x = Qubit(1)
+    print(f"|x>={x}")
+
+    y = Qubit(0)
+    print(f"|y>={y}")
+
+    def f(x, *args):
+        return 1
+
+    r = oracle(x, y, f)
+    print(r)
+
+    truth = TensorProduct(x, Qubit(f(1)))
+    truth = matrix_to_qubit(represent(truth))
+    assert truth == r
+
+
+def test_unitary_1():
+    # consider a superposed state
+    x = Qubit(1)
+    print(f"|x>={x}")
+
+    y = Qubit(0)
+    print(f"|y>={y}")
+
+    def f(x, *args):
+        return 1
+
+    r = oracle(x, y, f)
+    print(r)
+
+    truth = TensorProduct(x, Qubit(f(1)))
+    truth = matrix_to_qubit(represent(truth))
+    assert truth == r
+
+    r = oracle(x, Qubit(f(1)), f)
+    print(r)
+    truth = TensorProduct(x, y)
+    truth = matrix_to_qubit(represent(truth))
+    assert truth == r
+
+def test_unitary_2():
+    # consider a superposed state
+    x = Qubit(0)
+    print(f"|x>={x}")
+
+    y = qapply(HadamardGate(0) * Qubit(1))
+    print(f"|y>={y}")
+
+    def f(x, *args):
+        return 1
+
+    r = oracle(x, y, f)
+    print(r)
+
+    truth = TensorProduct((-1)**(f(x))*x, y)
+    truth = matrix_to_qubit(represent(truth))
+    assert truth == r
+
+    r = oracle((-1)**(f(x))*x, y, f)
+    print(r)
+    truth = TensorProduct(x, y)
+    truth = matrix_to_qubit(represent(truth))
+    assert truth == r

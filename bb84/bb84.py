ee198ce56807d682a2834dbcf39acd7160df8115
Guido Roncarolo <guido.roncarolo@gmail.com>
2024-09-28 16:30:53 +0200

    Improve BB84: add eavesdropping
    Signed-off-by: Guido Roncarolo <guido.roncarolo@gmail.com>

--- bb84/bb84.py
+++ bb84/bb84.py
@@ -3,7 +3,8 @@
 import numpy as np
 from sympy.physics.quantum.gate import HadamardGate
 from sympy.physics.quantum.qapply import qapply
-from sympy.physics.quantum.qubit import Qubit, measure_all
+from sympy.physics.quantum.qubit import Qubit
+from measure_all import measure_all_oneshot
 
 
 def get_random_bases(size=10):
@@ -24,17 +25,39 @@ def get_random_bits(size=10):
     return np.random.choice([0, 1], size=(size,))
 
 
-def bb84(get_alice_bits, get_alice_bases, get_bob_bases):
+def bb84(get_alice_bits, get_alice_bases, get_bob_bases, gen_bit_size=10, eavesdropping=False, check_size=0):
     """
     Run BB84 algorithm and return a private key
 
     :param func get_alice_bits: function that returns Alice bits
     :param func get_alice_bases: function that returns Alice bases
     :param func get_bob_bases: function that returns Bob bases
+    :param int gen_bit_size: how many bit we want to generate
+    :param Bool eavesdropping: simulate Eve eavesdropping
+    :param int check_size: how many bit of the key should be used to check eavesdropping
+
+
+    To make sure Eve did not eavesdrop Alice and Bob can generate
+    say 256 + 50 bits and share the 50 additional bits
+
+    Supposing Alice and Bob reveals 50 bits we can calculate the probability
+    they have to catch Eve eavesdropping
+                           Eve  ----------------- Bob     0.5 undetected
+                    0.5 /  Z, |0>                 Z,|0>
+    Alice   ---> Eve -
+    Z, |0>             \             ------------ Bob
+                    0.5 \          / 0.5          Z,|0>   0.25 undetcted
+                         \        /
+                          Eve---------
+                          X, |+>,|->  \
+                                       --------- Bob
+                                                 Z,|1>   0.25 Detected
+    On n bit the probability the Eve is not detected is
+    1 - (3/4)**n
     """
     # get bits and bases Alice side
-    alice_bits = get_alice_bits()
-    alice_bases = get_alice_bases()
+    alice_bits = get_alice_bits(gen_bit_size)
+    alice_bases = get_alice_bases(gen_bit_size)
     print(f"Alice bits: {alice_bits}")
     print(f"Alice bases: {alice_bases}")
 
@@ -44,25 +67,60 @@ def bb84(get_alice_bits, get_alice_bases, get_bob_bases):
         if base == 'X':
             qubit = qapply(HadamardGate(0) * qubit)
         alice_sent.append(qubit)
+    alice_sent = alice_sent
     print(f"Alice sent: {alice_sent}")
 
-    bob_bases = get_bob_bases()
+    if eavesdropping:
+        eve_bases = get_random_bases(gen_bit_size)
+        eavesdropped = []
+        for q, bb in zip(alice_sent, eve_bases):
+            qq = q
+            # change base if needed
+            if bb == 'X':
+                qq = qapply(HadamardGate(0) * q)
+
+            m = measure_all_oneshot(qq)
+            eavesdropped.append(m)
+
+        # measuring changes what alice_sent
+        alice_sent = eavesdropped
+
+    bob_bases = get_bob_bases(gen_bit_size)
     print(f"Bob bases: {bob_bases}")
 
     private_key = []
 
-    for q, bb, ab in zip(alice_sent, bob_bases, alice_bases):
+    for q, bb in zip(alice_sent, bob_bases):
         qq = q
         # change base if needed
         if bb == 'X':
             qq = qapply(HadamardGate(0) * q)
 
-        # if Alice and Bob bases match
-        # and the measure to the private key
-        if bb == ab:
-            m = measure_all(qq)
-            private_key.append(m[0][0])
-        else:
-            private_key.append('-')
+        m = measure_all_oneshot(qq)
+        private_key.append(m)
 
-    return private_key, alice_sent
+    # match bases
+    for ab, bb, i in zip(alice_bases, bob_bases, range(gen_bit_size)):
+        # if Alice and Bob bases match
+        # than the measure is good
+        if bb != ab:
+            private_key[i] = '-'
+
+    pk_size = sum(1 for x in private_key if x != '-')
+
+    print(f"private_key {private_key} size:{pk_size}")
+
+    eavesdropped = 0
+    if eavesdropping:
+        eavesdropped = 0
+        for pk, al in zip(private_key, alice_bits):
+            # the bases have to match
+            if pk != '-':
+                check_size -= 1
+                # the values too
+                if pk != Qubit(al):
+                    eavesdropped += 1
+            if check_size == 0:
+                break
+
+    return private_key, alice_sent, eavesdropped
--- bb84/test_bb84.py
+++ bb84/test_bb84.py
@@ -2,21 +2,23 @@ from bb84 import bb84, get_random_bases, get_random_bits
 from sympy.physics.quantum.qubit import Qubit
 from sympy import sqrt
 import numpy as np
+import math
+from sympy.physics.quantum.qubit import IntQubit, Qubit, measure_all_oneshot, measure_all, IntQubit, qubit_to_matrix
 
 
 def test_bb84():
     print()
 
-    def get_alice_bits():
+    def get_alice_bits(*args):
         return [0, 1, 0, 1, 1, 0, 1, 1, 1]
 
-    def get_alice_bases():
+    def get_alice_bases(*args):
         return ['Z', 'Z', 'X', 'Z', 'X', 'X', 'X', 'Z', 'Z']
 
-    def get_bob_bases():
+    def get_bob_bases(*args):
         return ['Z', 'X', 'X', 'Z', 'Z', 'X', 'Z', 'X', 'Z']
 
-    private_key, _ = bb84(get_alice_bits, get_alice_bases, get_bob_bases)
+    private_key, _, _ = bb84(get_alice_bits, get_alice_bases, get_bob_bases)
     truth = [Qubit(0), '-', Qubit(0), Qubit(1), '-', Qubit(0), '-', '-', Qubit(1)]
     assert private_key == truth
 
@@ -25,7 +27,7 @@ def test_bb84_1():
     print()
     np.random.seed(10)
 
-    private_key, _ = bb84(get_random_bits, get_random_bases, get_random_bases)
+    private_key, _, _ = bb84(get_random_bits, get_random_bases, get_random_bases)
     truth = [Qubit(1), Qubit(1), '-', Qubit(1), '-', Qubit(1), Qubit(1), Qubit(0), '-', Qubit(1)]
     assert private_key == truth
 
@@ -33,13 +35,13 @@ def test_bb84_1():
 def test_bb84_6_25():
     print()
 
-    def get_alice_bits():
+    def get_alice_bits(*args):
         return [1, 0, 0, 1, 0, 0, 0, 1, 1]
 
-    def get_alice_bases():
+    def get_alice_bases(*args):
         return ['X', 'X', 'Z', 'Z', 'Z', 'X', 'X', 'X', 'X']
 
-    def get_bob_bases():
+    def get_bob_bases(*args):
         return ['Z', 'X', 'X', 'Z', 'Z', 'X', 'Z', 'X', 'Z']
 
     plus = Qubit(0) / sqrt(2) + Qubit(1) / sqrt(2)
@@ -47,7 +49,7 @@ def test_bb84_6_25():
     zero = Qubit(0)
     one = Qubit(1)
 
-    private_key, alice_sent = bb84(get_alice_bits, get_alice_bases, get_bob_bases)
+    private_key, alice_sent, _ = bb84(get_alice_bits, get_alice_bases, get_bob_bases)
     truth = [minus, plus, zero, one, zero, plus, plus, minus, minus]
     assert truth == alice_sent
 
@@ -55,15 +57,33 @@ def test_bb84_6_25():
 def test_bb84_6_26():
     print()
 
-    def get_alice_bits():
+    def get_alice_bits(*args):
         return [0, 1, 0, 1, 0, 1, 1, 0, 1]
 
-    def get_alice_bases():
+    def get_alice_bases(*args):
         return ['X', 'X', 'Z', 'X', 'Z', 'Z', 'X', 'X', 'Z']
 
-    def get_bob_bases():
+    def get_bob_bases(*args):
         return ['Z', 'Z', 'Z', 'Z', 'Z', 'X', 'X', 'Z', 'Z']
 
-    private_key, _ = bb84(get_alice_bits, get_alice_bases, get_bob_bases)
-    truth = ['-', '-', Qubit(0), '-', Qubit(0), '-', Qubit(1), '-', Qubit(1)]
+    private_key, _, _ = bb84(get_alice_bits, get_alice_bases, get_bob_bases)
+    zero = Qubit(0)
+    one = Qubit(1)
+    truth = ['-', '-', zero, '-', zero, '-', one, '-', one]
     assert private_key == truth
+
+
+def test_bb84_eavesdropping_probability_detection():
+    print()
+
+    key_size = 256
+    check_size = 50
+
+    private_key, _, eavesdropped = bb84(get_random_bits, get_random_bases, get_random_bases,
+                                        gen_bit_size=key_size + check_size, eavesdropping=True, check_size=check_size)
+    prob = 1. - (3 / 4) ** check_size
+    print(f"prob to catch Eve: {prob} with {check_size} bits" )
+    print(f"Eve was caught {eavesdropped} times")
+
+    usable_pk = sum(1 if x != '-' else 0 for x in private_key)
+    print(f"Usable key len {usable_pk}")

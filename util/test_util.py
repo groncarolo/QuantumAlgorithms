bd548dfb0e6342322e87e18f163bb4f72d99583b
Guido Roncarolo <guido.roncarolo@gmail.com>
2024-11-14 17:02:00 +0100

    util: add more tests on change basis
    Signed-off-by: Guido Roncarolo <guido.roncarolo@gmail.com>

--- util/test_util.py
+++ util/test_util.py
@@ -25,5 +25,21 @@ def test_change_basis_2():
     q = sqrt(3) / 2 * Qubit('0') + 1 / 2 * Qubit('1')
     new_base = (i_state, i_minus_state)
     q_new = change_basis(q, new_base)
-    truth = (sqrt(3) -I) / (2 * sqrt(2)) * Qubit('0') + (sqrt(3) + I) / (2 * sqrt(2)) * Qubit('1')
+    truth = (sqrt(3) - I) / (2 * sqrt(2)) * Qubit('0') + (sqrt(3) + I) / (2 * sqrt(2)) * Qubit('1')
+    assert simplify(truth - q_new) == 0
+
+
+def test_change_basis_3():
+    q = (1 - 2 * I) / sqrt(6) * Qubit('0') + 1 / sqrt(6) * Qubit('1')
+    new_base = (plus, minus)
+    q_new = change_basis(q, new_base)
+    truth = (1 - I) / (sqrt(3)) * Qubit('0') - I / (sqrt(3)) * Qubit('1')
+    assert simplify(truth - q_new) == 0
+
+
+def test_change_basis_4():
+    q = (1 - 2 * I) / sqrt(6) * Qubit('0') + 1 / sqrt(6) * Qubit('1')
+    new_base = (i_state, i_minus_state)
+    q_new = change_basis(q, new_base)
+    truth = (1 - 3*I) / (2*sqrt(3)) * Qubit('0') + (1-I) / (2*sqrt(3)) * Qubit('1')
     assert simplify(truth - q_new) == 0

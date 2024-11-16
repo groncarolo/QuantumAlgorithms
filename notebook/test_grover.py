221784dd660def92233e10a62c05600437119f1b
Guido Roncarolo <guido.roncarolo@gmail.com>
2024-11-16 17:48:33 +0100

    Add Grover Algo
    Signed-off-by: Guido Roncarolo <guido.roncarolo@gmail.com>

---
+++ notebook/test_grover.py
@@ -0,0 +1,11 @@
+from testbook import testbook
+
+@testbook('grover.ipynb', execute=True)
+def test_0(testbook):
+    test = testbook.get('test_0')
+    test()
+
+@testbook('grover.ipynb', execute=True)
+def test_1(testbook):
+    test = testbook.get('test_1')
+    test()

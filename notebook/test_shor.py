f495670c6ee74edda904d8418988c706c6918ea9
Guido Roncarolo <guido.roncarolo@gmail.com>
2024-11-16 17:47:16 +0100

    add Shor factorization algo
    Signed-off-by: Guido Roncarolo <guido.roncarolo@gmail.com>

---
+++ notebook/test_shor.py
@@ -0,0 +1,21 @@
+from testbook import testbook
+
+@testbook('shor.ipynb', execute=True)
+def test_0(testbook):
+    test = testbook.get('test_0')
+    test()
+
+@testbook('shor.ipynb', execute=True)
+def test_1(testbook):
+    test = testbook.get('test_1')
+    test()
+
+@testbook('shor.ipynb', execute=True)
+def test_2(testbook):
+    test = testbook.get('test_2')
+    test()
+
+@testbook('shor.ipynb', execute=True)
+def test_3(testbook):
+    test = testbook.get('test_3')
+    test()

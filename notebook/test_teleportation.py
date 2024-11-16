46a1f0f420db80f1bad211ed5385c2e68245fbf9
Guido Roncarolo <guido.roncarolo@gmail.com>
2024-11-16 17:49:11 +0100

    Add Quantum Teleportation code
    Signed-off-by: Guido Roncarolo <guido.roncarolo@gmail.com>

---
+++ notebook/test_teleportation.py
@@ -0,0 +1,16 @@
+from testbook import testbook
+
+@testbook('teleportation.ipynb', execute=True)
+def test_0(testbook):
+    test = testbook.get('test_0')
+    test()
+
+@testbook('teleportation.ipynb', execute=True)
+def test_1(testbook):
+    test = testbook.get('test_1')
+    test()
+
+@testbook('teleportation.ipynb', execute=True)
+def test_2(testbook):
+    test = testbook.get('test_2')
+    test()

749cca33ee427044bc333c21346ef7c7454dcaef
Guido Roncarolo <guido.roncarolo@gmail.com>
2024-11-16 17:48:20 +0100

    Add Deutsch Algo
    Signed-off-by: Guido Roncarolo <guido.roncarolo@gmail.com>

---
+++ notebook/test_deutsch.py
@@ -0,0 +1,21 @@
+from testbook import testbook
+
+@testbook('deutsch.ipynb', execute=True)
+def test_0(testbook):
+    test = testbook.get('test_0')
+    test()
+
+@testbook('deutsch.ipynb', execute=True)
+def test_1(testbook):
+    test = testbook.get('test_1')
+    test()
+
+@testbook('deutsch.ipynb', execute=True)
+def test_2(testbook):
+    test = testbook.get('test_2')
+    test()
+
+@testbook('deutsch.ipynb', execute=True)
+def test_3(testbook):
+    test = testbook.get('test_3')
+    test()

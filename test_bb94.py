8a1f5fdaee2b55808b8eff815b43d44c02b41a59
Guido Roncarolo <guido.roncarolo@gmail.com>
2024-11-18 17:59:56 +0100

    Improve Test code
    Signed-off-by: Guido Roncarolo <guido.roncarolo@gmail.com>

--- test_bb94.py
+++ test_bb94.py
@@ -1,25 +1,29 @@
 from testbook import testbook
 
+
 @testbook('bb94.ipynb', execute=True)
 def test_0(testbook):
-
     test = testbook.get('test_0')
     test()
+
 
 @testbook('bb94.ipynb', execute=True)
 def test_1(testbook):
     test = testbook.get('test_1')
     test()
 
+
 @testbook('bb94.ipynb', execute=True)
 def test_2(testbook):
     test = testbook.get('test_2')
     test()
 
+
 @testbook('bb94.ipynb', execute=True)
 def test_3(testbook):
     test = testbook.get('test_3')
     test()
+
 
 @testbook('bb94.ipynb', execute=True)
 def test_4(testbook):
--- test_teleportation.py
+++ test_teleportation.py
@@ -1,14 +1,17 @@
 from testbook import testbook
 
+
 @testbook('teleportation.ipynb', execute=True)
 def test_0(testbook):
     test = testbook.get('test_0')
     test()
 
+
 @testbook('teleportation.ipynb', execute=True)
 def test_1(testbook):
     test = testbook.get('test_1')
     test()
+
 
 @testbook('teleportation.ipynb', execute=True)
 def test_2(testbook):
